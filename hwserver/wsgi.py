7c7ee10139b5a6feacda35cfef94eb14374af87e
naitianliu <naitianliu@gmail.com>
2016-09-14 18:42:18 -0700

    update approvals

--- hwserver/wsgi.py
+++ hwserver/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
+
 sys.path.append("/var/www/hwserver")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hwserver.settings")
 
 application = get_wsgi_application()
